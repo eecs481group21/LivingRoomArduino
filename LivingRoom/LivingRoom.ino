26c05b7cc9c1794526338852fe53b5e25788954a
Matt Cerny <mcerny@umich.edu>
2014-12-01 10:11:21 -0500

    changed input to analog for signal smoothing

--- LivingRoom/LivingRoom.ino
+++ LivingRoom/LivingRoom.ino
@@ -3,7 +3,7 @@
 int inPin = 12;        // the number of the input pin
 int inPinTwo = 11;     // the number of the second input pin
 int outPin = 13;       // the number of the output pin
-int inPinThree = 10;
+int inPinThree = A9;
 
 int state = HIGH;      // the current state of the output pin
 int reading;           // the current reading from the input pin
@@ -14,22 +14,25 @@ int previous = LOW;    // the previous reading from the input pin
 long time = 0;         // the last time the output pin was toggled
 long debounce = 200;   // the debounce time, increase if the output flickers
 
+int analogValue;
+
 void setup()
 {
   pinMode(inPin, INPUT_PULLUP);
   pinMode(inPinTwo, INPUT_PULLUP);
   pinMode(outPin, OUTPUT);
-  pinMode(inPinThree, INPUT_PULLUP);
+  analogReference(DEFAULT);
 }
 
 void loop()
 {
   reading = digitalRead(inPin);
-  
+
   // if the input just went from LOW and HIGH and we've waited long enough
   // to ignore any noise on the circuit, toggle the output pin and remember
   // the time
-  if (reading == HIGH && previous == LOW && millis() - time > debounce) {
+  if (reading == HIGH && previous == LOW && millis() - time > debounce) 
+  {
     if (state == HIGH)
       state = LOW;
     else
@@ -41,21 +44,41 @@ void loop()
   digitalWrite(outPin, state);
 
   previous = reading;
-  
-  if(digitalRead(inPinTwo) == LOW && state == HIGH){
+
+  if(digitalRead(inPinTwo) == LOW && state == HIGH)
+  {
     usb_keyboard_press(KEY_A, 0);
-    while(digitalRead(inPinTwo) == LOW){
+    while(digitalRead(inPinTwo) == LOW)
+    {
       delay(900);
-       //chill in here while the button is pressed  aa  
     }
   }
-  
-  if(digitalRead(inPinThree) == LOW && state == HIGH){
+
+  // inPinThree: Analog //////////////////////////////////////
+  for (int i = 0; i < 5; i++ )
+  {
+    analogValue += analogRead(inPinThree);
+    delay(10);
+  }
+  analogValue = analogValue/5;
+
+  if(analogValue > 900 && state == HIGH)
+  {
+    digitalWrite(13, HIGH);
     usb_keyboard_press(KEY_B, 0);
-    while(digitalRead(inPinThree) == LOW){
-      delay(900);
-       //chill in here while the button is pressed    
+
+    while(analogValue > 900)
+    {
+      analogValue = 0;
+      for (int i = 0; i < 5; i++ )
+      {
+        analogValue += analogRead(inPinThree);
+        delay(10);
+      }
+      analogValue = analogValue/5;
     }
   }
 }
+
+
 
